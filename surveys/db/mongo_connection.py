197194843d5960c5b852adfcc637a6f27c7ee47a
craigargh <craigargh@gmail.com>
2018-07-21 15:51:58 +0100

    Setup Kubernetes pods and DB service

--- surveys/db/mongo_connection.py
+++ surveys/db/mongo_connection.py
@@ -2,4 +2,4 @@ import mongoengine
 
 
 def startup():
-    mongoengine.register_connection(alias='main', name='surveys')
+    mongoengine.register_connection(alias='main', name='surveys', host='mongo')
